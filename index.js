42ce9efd69f7dc1030d77753b29c5ea2e1baaece
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-07-06 22:02:40 -0400

    made working connections for all view options in the app. still need add and edit features

--- index.js
+++ index.js
@@ -1,88 +1,189 @@
-const mysql = require('mysql');
-const cTable = require('console.table')
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
   port: 3306,
-  user: 'root',
-  password: '@Theo731',
-  database: 'cmsDB',
+  user: "root",
+  password: "@Theo731",
+  database: "cmsDB",
 });
 
-const readEmployees = () => {
-  console.log('Selecting all employees...\n');
-  connection.query('SELECT employee.id, first_name, last_name, title, department_name, salary, manager_id FROM employee INNER JOIN role ON employee.role_id=role.id INNER JOIN department ON role.department_id=department.id;', (err, res) => {
-    if (err) throw err;
-    // Log all results of the SELECT statement
-    console.table(res);
-    connection.end();
-  });
+const viewAllEmployees = () => {
+  connection.query(
+    "SELECT employee.id, first_name, last_name, title, department_name, salary, manager_id FROM employee INNER JOIN role ON employee.role_id=role.id INNER JOIN department ON role.department_id=department.id;",
+    (err, res) => {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.table(res);
+      start();
+    }
+  );
+};
+const viewAllRoles = () => {
+  connection.query(
+    "SELECT * FROM cmsDB.role INNER JOIN Department ON role.department_id=department.id;",
+    (err, res) => {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.table(res);
+      start();
+    }
+  );
+};
+const viewAllDepartments = () => {
+  connection.query(
+    "SELECT * FROM cmsDB.department;",
+    (err, res) => {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.table(res);
+      start();
+    }
+  );
 };
 
-// const deleteProduct = () => {
-//   console.log('Deleting all strawberry icecream...\n');
-//   connection.query(
-//     'DELETE FROM products WHERE ?',
-//     {
-//       flavor: 'strawberry',
-//     },
-//     (err, res) => {
-//       if (err) throw err;
-//       console.log(`${res.affectedRows} products deleted!\n`);
-//       // Call readProducts AFTER the DELETE completes
-//       readProducts();
-//     }
-//   );
-// };
+const viewEmployeesByDepartment = () => {
 
-// const updateProduct = () => {
-//   console.log('Updating all Rocky Road quantities...\n');
-//   const query = connection.query(
-//     'UPDATE products SET ? WHERE ?',
-//     [
-//       {
-//         quantity: 100,
-//       },
-//       {
-//         flavor: 'Rocky Road',
-//       },
-//     ],
-//     (err, res) => {
-//       if (err) throw err;
-//       console.log(`${res.affectedRows} products updated!\n`);
-//       // Call deleteProduct AFTER the UPDATE completes
-//       deleteProduct();
-//     }
-//   );
+  connection.query('SELECT * FROM Department', (err, results) => {
+    if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to bid on
+    inquirer
+      .prompt([
+        {
+          name: 'choice',
+          type: 'rawlist',
+          choices() {
+            const choiceArray = [];
+            results.forEach(({ department_name }) => {
+              choiceArray.push(department_name);
+            });
+            return choiceArray;
+          },
+          message: 'Which department would you like to view?',
+        },
+      ])
+      .then((answer) => {
+        // get the information of the chosen item
+        let chosenItem;
+        results.forEach((item) => {
+          // console.log(item.department_name);
+          // console.log(answer);
+          if (item.department_name === answer.choice) {
+            chosenItem = item;
+            // console.log(chosenItem);
+          }
+        });
+        const query = 
+      connection.query('SELECT employee.id, first_name, last_name, title, department_name, salary, manager_id FROM employee INNER JOIN role ON employee.role_id=role.id INNER JOIN department ON role.department_id=department.id WHERE ?',
+      {
+        department_name: chosenItem.department_name
+      }, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
 
-//   // logs the actual query being run
-//   console.log(query.sql);
-// };
+      })
+    })
+  })
+}
 
-// const createEmployee = () => {
-//   console.log('Inserting a new employee...\n');
-//   const query = connection.query(
-//     'INSERT INTO employee SET ?',
-//     {
-//       flavor: 'Rocky Road',
-//       price: 3.0,
-//       quantity: 50,
-//     },
-//     (err, res) => {
-//       if (err) throw err;
-//       console.log(`${res.affectedRows} product inserted!\n`);
-//       // Call updateProduct AFTER the INSERT completes
-//       updateProduct();
-//     }
-//   );
+const start = () => {
+  inquirer
+    .prompt({
+      name: "toDo",
+      type: "list",
+      message: "What would you like to?",
+      choices: [
+        "View all Employees",
+        "View all Employees By Department",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add a Role",
+        "View All Departments",
+        "Add Department",
+        "EXIT",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.toDo) {
+        case "View all Employees":
+          viewAllEmployees();
+          break;
 
-//   // logs the actual query being run
-//   console.log(query.sql);
-// };
+        case "View all Employees By Department":
+          viewEmployeesByDepartment();
+          break;
+
+        case "Add Employee":
+          viewAllEmployees();
+          break;
+
+        case "Update Employee Role":
+          viewAllEmployees();
+          break;
+        case "View All Roles":
+          viewAllRoles();
+          break;
+
+        case "Add a Role":
+          viewAllEmployees();
+          break;
+
+        case "View All Departments":
+          viewAllDepartments();
+          break;
+
+        case "Add Departments":
+          viewAllEmployees();
+          break;
+
+        case "EXIT":
+          connection.end();
+          break;
+
+        default:
+          console.log(`Invalid action: ${answer.toDo}`);
+          break;
+      }
+    });
+};
+const newEmployee = () => {
+  inquirer.prompt([
+    {
+      name: "firstName",
+      type: "input",
+      message: "What is the employee's first name?",
+    },
+    {
+      name: "lastName",
+      type: "input",
+      message: "What is the employee's last name?",
+    },
+  ]);
+  console.log("Inserting a new employee...\n");
+  const query = connection.query(
+    "INSERT INTO employee SET ?",
+    {
+      flavor: "Rocky Road",
+      price: 3.0,
+      quantity: 50,
+    },
+    (err, res) => {
+      if (err) throw err;
+      console.log(`${res.affectedRows} product inserted!\n`);
+      // Call updateProduct AFTER the INSERT completes
+      updateProduct();
+    }
+  );
+};
+
 
 // Connect to the DB
 connection.connect((err) => {
   if (err) throw err;
   console.log(`connected as id ${connection.threadId}\n`);
-  readEmployees();
+  start();
 });
